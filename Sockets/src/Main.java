64c6d839e33a9db50eaaa7665dc9fd44af3acc5e
jorgebonilla2504 <70659806+jorgebonilla2504@users.noreply.github.com>
2020-12-23 22:39:51 -0600

    Update Main.java

--- Sockets/src/Main.java
+++ Sockets/src/Main.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
+        // Hilo para sockets, creo un nuevo marco para conexion
         marco i = new marco();
         i.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
